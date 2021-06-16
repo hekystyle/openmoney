81288769f477345ea08507ab2e6eabc783bf9c39
Tomáš Hekrla <hekrla@enmon.tech>
2021-06-16 08:23:17 +0200

    setup simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
+import express from "express";
 
-throw new Error('DEV');
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!" + (new Date().toISOString()));
+});
+
+app.get("/e", () => {
+  throw new Error('DEV');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
