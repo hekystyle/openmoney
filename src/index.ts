996e8865e84f4728fecb9e7f02bee2466ee65ac1
Tomáš Hekrla <hekrla@enmon.tech>
2021-06-20 11:20:41 +0200

    add koa-router

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import Koa from "koa";
+import Router from "koa-router";
 
 const PORT = 3000;
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = "Hello World!" + (new Date().toISOString());
+const router = new Router();
+router.get('/', async ctx => {
+  ctx.body = `[${new Date().toISOString()}] Server is running and healthy.`;
 });
+
+app.use(router.middleware());
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
