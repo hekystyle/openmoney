fcae916da049db6c15e1ae6475618a6439a54a8e
Tomáš Hekrla <hekrla@enmon.tech>
2021-11-15 11:30:12 +0100

    refactor: cleanup

--- src/models/account.ts
+++ src/models/account.ts
@@ -6,14 +6,6 @@ export interface Account {
   currency: string;
 }
 
-export function isAccount(val: any): val is Account {
-  return (
-    val !== null
-    && typeof val === 'object'
-    && typeof val.name === 'string'
-  );
-}
-
 export const accountSchema = new Schema<Account>({
   name: { type: String, required: true },
   balance: { type: Number, required: true },
--- src/models/category.ts
+++ src/models/category.ts
@@ -4,14 +4,6 @@ export interface Category {
   name: string;
 }
 
-export function isCategory(val: any): val is Category {
-  return (
-    val !== null
-    && typeof val === 'object'
-    && typeof val.name === 'string'
-  );
-}
-
 export const categorySchema = new Schema<Category>({
   name: { type: String, required: true },
 });
